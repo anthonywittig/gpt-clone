605fb88bd4496d5dca6ee81663a5f8c9cf06c4f0
Anthony Wittig <anthony.wittig@gmail.com>
2023-05-18 23:06:05 -0600

    hello world

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from "react";
+
+interface Message {
+  title: string;
+  role: string;
+  content: string;
+}
+
+const App = () => {
+  const [message, setMessage] = useState<Message | null>(null);
+  const [value, setValue] = useState("");
+  const [previousChats, setPreviousChats] = useState<Message[]>([]);
+  const [currentTitle, setCurrentTitle] = useState<string | null>(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitle: string) => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(uniqueTitle);
+  };
+
+  const getMessages = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/completions", {
+        method: "POST",
+        body: JSON.stringify({
+          message: value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setMessage(data.choices[0].message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => {
+        return [
+          ...prevChats,
+          { title: currentTitle, role: "user", content: value },
+          { title: currentTitle, role: message.role, content: message.content },
+        ];
+      });
+    }
+  }, [message, currentTitle]);
+
+  const currentChat = previousChats.filter(
+    (previousChat) => previousChat.title === currentTitle
+  );
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChat) => previousChat.title))
+  );
+
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button onClick={createNewChat}>+ New chat</button>
+        <ul className="history">
+          {uniqueTitles.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
+        </ul>
+        <nav>
+          <p>Made by aw</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>awGPT</h1>}
+        <ul className="feed">
+          {currentChat.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              Go
+            </div>
+          </div>
+          <p className="info">whatever</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+
+const PORT = 8000;
+const app = express();
+config();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/completions", async (req: Request, res: Response) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: req.body.message }],
+      max_tokens: 100,
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
